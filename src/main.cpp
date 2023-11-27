f80466d90a5716b6ec9d1c49aab7371e871a933a
mcthouacbb <mcthouacbb@gmail.com>
2023-11-27 16:48:40 -0500

    Added support for printing eval params (#3)

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,10 @@ int main()
     std::cout << "K Value: " << kValue << std::endl;
     std::cout << calcError(data.positions, data.allCoefficients, kValue, defaultParams) << std::endl;
     std::vector<Gradient> gradients(defaultParams.size());
-    computeGradient(data.positions, data.allCoefficients, kValue, defaultParams, gradients);
-    for (int i = 0; i < gradients.size(); i++)
-        std::cout << "{" << gradients[i].mg << ' ' << gradients[i].eg << "}, ";
+    computeGradient(data.positions, data.allCoefficients, 0.05, defaultParams, gradients);
+    EvalFn::printEvalParams(gradients);
+    EvalFn::printEvalParamsExtracted(defaultParams);
+    // for (int i = 0; i < gradients.size(); i++)
+        // std::cout << "{" << gradients[i].mg << ' ' << gradients[i].eg << "}, ";
     std::cout << "\n\n";
 }
